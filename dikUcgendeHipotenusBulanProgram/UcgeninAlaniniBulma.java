fafd80365594bd17557c6b0ba816525731411f2a
Samed Degirmenci <degirmencismd@gmail.com>
2023-03-28 23:37:24 +0300

    UcgeninAlani

--- dikUcgendeHipotenusBulanProgram/UcgeninAlaniniBulma.java
+++ dikUcgendeHipotenusBulanProgram/UcgeninAlaniniBulma.java
@@ -1,4 +1,26 @@
 package dikUcgendeHipotenusBulanProgram;
 
+import java.util.Scanner;
+
 public class UcgeninAlaniniBulma {
+    public static void main(String[] args) {
+        int a, b, c ;
+        double alan,u ;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Lutfen ucgenin 1. kenarini giriniz");
+        a=input.nextInt();
+
+        System.out.println("Lutfen ucgenin 2. kenarini giriniz");
+        b=input.nextInt();
+
+        System.out.println("Lutfen ucgenin 3. kenarini giriniz");
+        c=input.nextInt();
+
+        u=(a+b+c)/2;
+
+        alan=Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println("Ucgenin alani = " + alan);
+
+
+    }
 }
