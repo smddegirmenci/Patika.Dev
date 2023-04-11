4a652e3521d0e269ced45f1186622c542a60fcb5
Samed Degirmenci <degirmencismd@gmail.com>
2023-04-11 23:15:30 +0300

    YildizlarIleElmas

---
+++ yildizlarIleSekilCizmeProgrami/YildizlarIleElmasYapimi.java
@@ -0,0 +1,34 @@
+package yildizlarIleSekilCizmeProgrami;
+
+import java.util.Scanner;
+
+public class YildizlarIleElmasYapimi {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen basamak sayisini giriniz");
+        int n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * (n-i)) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
