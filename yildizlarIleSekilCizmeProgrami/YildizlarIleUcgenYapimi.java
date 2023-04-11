37a861ca1e8c19d431eb2cacc49febdd83680c56
Samed Degirmenci <degirmencismd@gmail.com>
2023-04-11 23:08:53 +0300

    YildizlarIleUcgen

--- yildizlarIleSekilCizmeProgrami/YildizlarIleUcgenYapimi.java
+++ yildizlarIleSekilCizmeProgrami/YildizlarIleUcgenYapimi.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class YildizlarIleUcgenYapimi {
     public static void main(String[] args) {
-        Scanner scan =new Scanner(System.in);
-        System.out.println("Lutfen basamak sayisini giriniz");
-        int n =scan.nextInt();
 
-        for (int i = 1; i <=n ; i++) {
-            for (int j = 1; j <=(n-i) ; j++) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen basamak sayisini giriniz");
+        int n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
             }
 
-            for (int k = 1; k <=(2*i)-1 ; k++) {
+            for (int k = 1; k <= (2 * i) - 1; k++) {
                 System.out.print("*");
             }
             System.out.println();
